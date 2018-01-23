47a91e4a974a3a8301743ca511204e7d9fe0c414
Ren Junlin <junlin_ren@foxmail.com>
2018-01-23 22:51:38 +0800

    init commit

--- src/containers/city/index.jsx
+++ src/containers/city/index.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+import {browserHistory} from 'react-router'
+import * as userInfoActionsFromOtherFile from '../../actions/userInfo'
+import Header from '../../components/header'
+import CurrentCity from '../../components/currentCity'
 
-class City extends React.Component{
-  constructor(props,context){
-    super(props,context)
-    this.shouldComponentUpdate=PureRenderMixin.shouldComponentUpdate()
+class City extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate()
   }
-  render(){
+
+  render() {
     return (
       <div>
-        <h1>city</h1>
+        <Header title="选择城市"/>
+        <CurrentCity cityName={this.props.userInfo.cityName}/>
       </div>
     )
   }
+
+  componentDidMount() {
+
+  }
 }
 
-export default  City
+function mapStateToProps(state) {
+  return {
+    userInfo: state.userInfo
+  }
+}
+
+function mapDispathToProps(dispatch) {
+  return {
+    userInfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch)
+  }
+}
+
+export default  connect(
+  mapStateToProps,
+  mapDispathToProps
+)(City)
